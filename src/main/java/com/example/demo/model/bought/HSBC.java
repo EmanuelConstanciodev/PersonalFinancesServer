986fcbaef8ab0afcf321f58dfc08c54b2ae148f2
EXC55284 <ian.crespi@claro.com.ar>
2024-08-21 00:43:17 -0300

    changing on card

--- src/main/java/com/example/demo/model/bought/HSBC.java
+++ src/main/java/com/example/demo/model/bought/HSBC.java
@@ -1,14 +1,25 @@
 package com.example.demo.model.bought;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class HSBC implements Card {
 
   @Override
   public LocalDate getNextClosingDate(LocalDate date) {
+    date.getMonth().getY;
+    YearMonth yearMonth = YearMonth.of(date.getYear(), date.getMonth());
+    LocalDate closingDate = getClosingDateOfAMonth(yearMonth);
     //PRIMER JUEVES SI TIENE 3 O MAS DIAS HABILES ANTES
     // SI NO, SEGUNDO JUEVES
     return null;
+  }
+
+  private LocalDate getClosingDateOfAMonth(YearMonth yearMonth) {
+    for (int i = 0 ; i < 7; i++) {
+
+    }
+    yearMonth.atDay()
   }
 
   @Override
